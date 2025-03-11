82e0b9de7d7c7726fbb65930f800dc26c0515470
Gustavo Henrique <145779028+hgrgustavo@users.noreply.github.com>
2025-03-10 21:59:29 -0300

    Add files via upload

--- website/static/js/scripts.js
+++ website/static/js/scripts.js
@@ -1,1 +1,17 @@
+function dropdown() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsById("dropdown-conttent");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 

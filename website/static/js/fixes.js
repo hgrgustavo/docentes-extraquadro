80304d8a917ad8b44f5b27a7605dfa6a40c68191
gus <gustavohgrdev@gmaiil.com>
2025-04-22 01:45:53 -0300

    feat: cpf and cnpj fields dynamic constraint

--- website/static/js/fixes.js
+++ website/static/js/fixes.js
@@ -2,6 +2,7 @@ class Fixes {
   constructor() {
     this.removeSelectDashes();
     this.dropdownClick();
+    this.selectConstraint();
   }
 
   removeSelectDashes() {
@@ -14,7 +15,6 @@ class Fixes {
         console.error("O elemento select não foi encontrado ou está vazio.");
       }
     })
-
   }
 
   dropdownClick() {
@@ -27,6 +27,59 @@ class Fixes {
         dropdown.classList.toggle("hidden");
       });
     })
+  }
+  // Constraints
+  constraintInputField(field_id) {
+    field_id.readOnly = true;
+    field_id.ariaReadOnly = true;
+    field_id.required = false;
+    field_id.setAttribute("placeholder", "bloqueado");
+  }
+
+  releaseInputField(field_id) {
+    field_id.required = true;
+    field_id.ariaRequired = true;
+    field_id.readOnly = false;
+    field_id.ariaReadOnly = false;
+    field_id.setAttribute("placeholder", "obrigatório");
+  }
+
+  selectConstraint() {
+    document.addEventListener("DOMContentLoaded", () => {
+      const selectElement = document.getElementById("pf_ou_pj");
+      const cnpjInput = document.getElementById("cnpj");
+      const cpfInput = document.getElementById("cpf");
+
+      if (!selectElement || !cnpjInput || !cpfInput) {
+        console.error("Erro: Elementos do formulário não encontrados.");
+        return;
+      }
+
+      selectElement.addEventListener("change", () => {
+        const currentSelection = selectElement.selectedOptions[0];
+
+        switch (currentSelection) {
+          case selectElement.options[0]:
+            this.constraintInputField(cnpjInput);
+            this.releaseInputField(cpfInput);
+
+            break;
+
+          case selectElement.options[1]:
+            this.constraintInputField(cpfInput);
+            this.releaseInputField(cnpjInput);
+
+            break;
+
+          default:
+            console.error("Seleção inválida");
+        }
+      });
+
+      if (selectElement) {
+        selectElement.dispatchEvent(new Event("change"));
+      }
+    });
   }
 }
 
